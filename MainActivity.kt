0ebed9e1f693a47da2a1eff378d8d5b578483863
Destiny_Ed <dikeachaeze@gmail.com>
2020-01-01 09:06:41 -0800

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.splashtutorial  //Make sure your change the package name to your apps package name
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
